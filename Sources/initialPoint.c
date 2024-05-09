3963da4676bbb934fe26ea18096c8ebfb0992f22
Denis Espanhol <denisgfespanhol@gmail.com>
2024-05-09 13:08:09 -0300

    Creating initialPoint file

---
+++ Sources/initialPoint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+void firstPointInit()
+{
+    printf("Hello David\n");
+}
