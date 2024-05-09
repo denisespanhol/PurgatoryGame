80bbd32760109c1b1d25ce7071c7b51294b0b1d6
Denis Espanhol <denisgfespanhol@gmail.com>
2024-05-09 11:40:52 -0300

    declaring the variables to the playerName and the confirmation

--- Sources/main.c
+++ Sources/main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <locale.h>
 
+#define maxCaracPlayer 11
+
 int main() {
     // Seta a localização do projeto para português
     setlocale(LC_ALL, "Portuguese");
+
+    char playerName[maxCaracPlayer];
+    char yesOrNo;
 
     return 0;
 }
