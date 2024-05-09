9eef94b32eb8b2cfa49a505b679cd0c2245b6a4e
Denis Espanhol <denisgfespanhol@gmail.com>
2024-05-09 12:19:26 -0300

    creates the function InsertPlayerName in main.c

--- Sources/main.c
+++ Sources/main.c
@@ -4,6 +4,8 @@
 
 #define maxCaracPlayer 11
 
+void InsertPlayerName(char playerName[maxCaracPlayer]);
+
 int main() {
     // Seta a localização do projeto para português
     setlocale(LC_ALL, "Portuguese");
@@ -11,5 +13,17 @@ int main() {
     char playerName[maxCaracPlayer];
     char yesOrNo;
 
+    printf("Olá! Antes de iniciar sua aventura, nos diga como gostaria de ser chamado! (Máx. 10 carecteres)\n");
+    
     return 0;
+}
+
+void InsertPlayerName(char playerName[maxCaracPlayer])
+{
+    printf("\nInsira seu nome, ou apelido, aqui: ");
+    fgets(playerName, 10, stdin);
+
+    // Remove the fgets \n. Source: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+    playerName[strcspn(playerName, "\n")] = 0;
+    fflush(stdin);
 }
